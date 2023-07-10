a9be05752d6bae8f676c4d78bf80dfa84bb85105
这不是 Hack 而是技能 <81523685+ViniciusRed@users.noreply.github.com>
2023-07-10 13:59:59 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,7 @@
 
 # export PACKAGER="AntMan666 <945360554@qq.com>"
 
-git clone "https://aur.archlinux.org/$1.git"
-cd "$1"
+git clone "$1" && cd "$(basename "$1" .git)"
 echo 'PACKAGER="AntMan666 <antman666@qq.com>"' >> /etc/makepkg.conf
 updpkgsums
 makepkg -sf --noconfirm --skippgpcheck
