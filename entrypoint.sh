ee2fca24e6d0843f6f97710efe2161c396cc4c63
viniciusred <atalibavinicius@gmail.com>
2025-09-27 18:37:46 -0300

    feat(ci): add multilib support and package installation
    Add support for enabling multilib repository and installing additional packages.
    The `enable-multilib` input enables the multilib repository in pacman.conf when set to true.
    The `install-packages` input allows specifying additional packages to install.

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,20 @@ if [ ! -z "$2" ]; then
     /setup-repo.sh "$2"
 fi
 
+# Enable multilib if requested
+if [ "$3" == "true" ]; then
+    echo "Enabling multilib repository..."
+    # Uncomment multilib lines in pacman.conf
+    sed -i '/^#\[multilib\]/,/^#Include = \/etc\/pacman.d\/mirrorlist/ s/^#//' /etc/pacman.conf
+    pacman -Sy
+fi
+
+# Install additional packages if provided
+if [ ! -z "$4" ]; then
+    echo "Installing additional packages: $4"
+    pacman -S --noconfirm $4
+fi
+
 if [[ $1 == http* ]]; then
     # Handle Git URL
     git clone "$1" && cd "$(basename "$1" .git)"
